14bb0024cd8f8a14c5c0afd06b6223986363cb8a
kotaro <kotaro.matsuhana@rakus-partners.co.jp>
2026-02-18 11:51:12 +0900

    最初のコミット

---
+++ src/main/java/com/exbbs/ex_bbs/ExBbsApplication.java
@@ -0,0 +1,13 @@
+package com.exbbs.ex_bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBbsApplication.class, args);
+	}
+
+}
